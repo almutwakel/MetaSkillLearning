686e28a03d9e8f9eebfb4547e5524650394d1973
tanmayshankar <tanmayshankar@github.com>
2020-04-02 17:56:59 -0400

    Adding trial with sys path append

--- Experiments/Trial.py
+++ Experiments/Trial.py
@@ -1,6 +1,7 @@
 import torch, torch.nn as nn, numpy as np, sys
 sys.path.append("~/Research/Code/spinningup")
 # sys.path.append("/private/home/tanmayshankar/Research/Code/spinningup")
+embed()
 from spinup.exercises.pytorch.problem_set_1 import exercise1_1
 from spinup.exercises.pytorch.problem_set_1 import exercise1_2_auxiliary
 
