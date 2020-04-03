ff5fe5e2587c0c8f9cdc36f5d8e3fad5a0ca0ecd
tanmayshankar <tanmayshankar@github.com>
2020-04-03 12:00:22 -0400

    Adding cluster run and run log

---
+++ Experiments/Run_Logs/Run_Log.py
@@ -0,0 +1,4 @@
+# Execute from MetaSkillLearning/Experiments directory: 
+
+python cluster_run.py --partition=learnfair --name=RL1 --cmd='python Run_Robosuite_PPO.py --env="SawyerLift" --run_name="1"'
+
